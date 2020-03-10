ee31a06388b1f4cd33fc298fa278e9b51b4c8731
Richard Hessinger <rhessinger@inedo.com>
2020-03-10 15:04:17 -0400

    Initial POC of clair vulnerability scanning for ProGet 5.3+

---
+++ InedoExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.Extensibility;
+
+[assembly: AssemblyTitle("Clair")]
+[assembly: AssemblyDescription("Contains a Vulnerability Source which connects to Clair's API for static analysis of vulnerabilities in application containers.")]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("ProGet")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2020")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: ComVisible(false)]
+
+[assembly: AppliesTo(InedoProduct.ProGet)]
