7afe6fc798c6f7c30d870783700ef3a95f20e015
Richard Hessinger <rhessinger@inedo.com>
2022-05-19 16:44:11 -0400

    Upgraded to .NET 6 and Inedo.SDK 2.0.0
    closes #4

--- Clair/InedoExtension/Properties/AssemblyInfo.cs
+++ Clair/InedoExtension/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using Inedo.Extensibility;
 [assembly: AssemblyDescription("Contains a Vulnerability Source which connects to Clair's API for static analysis of vulnerabilities in application containers.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("ProGet")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2020")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2022")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: ComVisible(false)]
 
 [assembly: AppliesTo(InedoProduct.ProGet)]
